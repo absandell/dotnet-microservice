0f8dd62e25e971a2dd6daa501eb25faaf61492dc
absandell <absandell@gmail.com>
2024-02-16 21:15:44 -0500

    Command Controllers

--- CommandsService/Controllers/CommandsController.cs
+++ CommandsService/Controllers/CommandsController.cs
@@ -21,7 +21,32 @@ namespace CommandsService.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
         {
-            return Ok();
+            Console.WriteLine($"--> Hit GetCommandsForPlatform: {platformId}");
+            if(!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+            var commands = _repository.GetCommandsForPlatform(platformId);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId)
+        {
+            Console.WriteLine($"--> Hit GetCommandForPlatform: {platformId} / {commandId}");
+            if(!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+
+            var command = _repository.GetCommand(platformId, commandId);
+
+            if(command == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<CommandReadDto>(command));
         }
     }
 }
